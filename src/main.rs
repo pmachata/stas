d82afa3a484dc05aee11f0bea36aa56a7cd05d85
Petr Machata <pmachata@gmail.com>
2019-10-08 00:43:41 +0200

    Match interfaces using glob
    Read list of interfaces from netlink, interpret the command line argument
    as a glob and match an interface from the list.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
-extern crate nix;
-extern crate libc;
 extern crate alloc;
+extern crate libc;
+extern crate neli;
+extern crate nix;
+extern crate glob;
+extern crate termion;
 
 use std::env;
 use std::ffi::CStr;
+use std::io::{stdout, Write};
 use std::mem;
 use std::os::unix::io::RawFd;
+use std::thread;
+use std::time;
+
 use libc::{c_void, c_char};
+
 use nix::sys::socket;
-use nix::unistd::close;
+//use nix::unistd::close;
 use nix::sys::socket::{AddressFamily, SockType, SockFlag};
+
+use neli::consts::*;
+use neli::nl::Nlmsghdr;
+use neli::rtnl::*;
+use neli::socket::*;
+//use neli::err::*;
 
 fn open_sock() -> RawFd {
     match socket::socket(AddressFamily::Inet, SockType::Datagram, SockFlag::empty(), None) {
@@ -176,30 +190,98 @@ fn ethtool_ss_stats_values(fd: RawFd, ifname: &String, len: u32) -> Vec<u64> {
     statvalues
 }
 
+fn ifla_link_name(ifi: Nlmsghdr<u16, Ifinfomsg<Ifla>>) -> Option<String> {
+    for attr in ifi.nl_payload.rtattrs {
+        let payload: Vec<u8> = attr.rta_payload;
+        match attr.rta_type {
+            Ifla::Ifname => {
+                // Snip terminating zero.
+                let prefix = &payload[.. (payload.len() - 1)];
+                return Some(String::from_utf8_lossy(prefix).into_owned())
+            },
+            _ => break,
+        }
+    }
+
+    None
+}
+
 fn main() {
-    let ifname = {
+    let ifmatch = {
         let mut args: Vec<String> = env::args().collect();
         if args.len() != 2 {
             println!("Usage: {} <if>", args[0]);
             std::process::exit(1);
         }
-        let ifname = args.remove(1);
-        if ifname.bytes().len() > IFNAMSIZ - 1 {
-            println!("{}: name too long", ifname);
+
+        let arg1 = args.remove(1);
+        if let Ok(ifmatch) = glob::Pattern::new(&arg1) {
+            ifmatch
+        } else {
+            println!("Interface match expected, e.g. 'eth0' or 'eth*'.");
             std::process::exit(1);
         }
-        ifname
     };
 
-    let fd = open_sock();
-    let len = ethtool_ss_stats_len(fd, &ifname);
-    println!("{} has {} 'ethtool -S' strings", ifname, len);
+    let ifnames = {
+        let mut socket = NlSocket::connect(NlFamily::Route, None, None, true).unwrap();
+        let ifim: Ifinfomsg<Ifla> = {
+            let ifi_family = RtAddrFamily::from(0);
+            let ifi_type = Arphrd::Ether;
+            let ifi_index = 0;
+            let ifi_flags = vec![];
+            let rtattrs = vec![];
+            Ifinfomsg::new(ifi_family, ifi_type, ifi_index, ifi_flags, rtattrs)
+        };
+        let nlhdr = {
+            let len = None;
+            let nl_type = Rtm::Getlink;
+            let flags = vec![NlmF::Request, NlmF::Dump];
+            let seq = None;
+            let pid = None;
+            let payload = ifim;
+            Nlmsghdr::new(len, nl_type, flags, seq, pid, payload)
+        };
 
-    let statnames = ethtool_ss_stats_names(fd, &ifname, len);
-    let statvalues = ethtool_ss_stats_values(fd, &ifname, len);
-    for (name, value) in statnames.iter().zip(statvalues.iter()) {
-        println!("{}\t{}", name, value);
+        socket.send_nl(nlhdr).unwrap();
+
+        let mut ifnames = Vec::<String>::new();
+        while let Ok(nl) = socket.recv_nl::<u16, Ifinfomsg<Ifla>>(None) {
+            if let Some(ifname) = ifla_link_name(nl) {
+                ifnames.push(ifname);
+            }
+        }
+        ifnames
+    };
+
+    loop {
+        print!("{}", termion::clear::All);
+        let mut line = 1;
+
+        for ifname in &ifnames {
+            if !ifmatch.matches(&ifname) {
+                continue;
+            }
+
+            println!("== {} ==", ifname);
+            line += 1;
+            let fd = open_sock();
+            let len = ethtool_ss_stats_len(fd, &ifname);
+            let statnames = ethtool_ss_stats_names(fd, &ifname, len);
+
+            let statvalues = ethtool_ss_stats_values(fd, &ifname, len);
+            for (name, value) in statnames.iter().zip(statvalues.iter()) {
+                print!("{}{}{}\t{}",
+                       termion::cursor::Goto(1, line as u16),
+                       termion::clear::CurrentLine,
+                       name, value);
+                line += 1;
+            }
+        }
+
+        stdout().flush().unwrap();
+        thread::sleep(time::Duration::from_millis(200));
     }
 
-    close(fd).unwrap();
+    //close(fd).unwrap();
 }
