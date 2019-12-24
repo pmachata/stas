216a581c98916f9cbc183b95d2d68444a4a60c0d
Petr Machata <pmachata@gmail.com>
2019-12-24 01:30:29 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -10,43 +10,58 @@ mod ethtool_ss;
 mod netlink;
 
 fn main() {
-    let ifmatch = {
+    let ifmatch: glob::Pattern;
+    let ctmatch: Vec::<glob::Pattern>;
+    {
         let mut args: Vec<String> = env::args().collect();
-        if args.len() != 2 {
-            println!("Usage: {} <if>", args[0]);
+        if args.len() <= 2 {
+            println!("Usage: {} <if> <counter> [<counter> ...]", args[0]);
             std::process::exit(1);
         }
 
         let arg1 = args.remove(1);
-        if let Ok(ifmatch) = glob::Pattern::new(&arg1) {
-            ifmatch
+        if let Ok(pat) = glob::Pattern::new(&arg1) {
+            ifmatch = pat;
         } else {
             println!("Interface match expected, e.g. 'eth0' or 'eth*'.");
             std::process::exit(1);
         }
-    };
+
+	let mut _ctmatch = Vec::<glob::Pattern>::new();
+	for arg in args {
+	    if let Ok(pat) = glob::Pattern::new(&arg) {
+		_ctmatch.push(pat);
+	    } else {
+		println!("Counter match expected, e.g. 'tx_bytes' or 'tx_*'.");
+		std::process::exit(1);
+	    }
+	}
+	ctmatch = _ctmatch;
+    }
 
     loop {
-        print!("{}", termion::clear::All);
+	print!("{}", termion::clear::All);
         let mut line = 1;
-
         for ifname in netlink::ifnames() {
             if !ifmatch.matches(&ifname) {
                 continue;
             }
 
-            println!("== {} ==", ifname);
-            line += 1;
             for ethtool_ss::Stat{name, value} in ethtool_ss::stats_for(&ifname) {
-                print!("{}{}{}\t{}",
-                       termion::cursor::Goto(1, line as u16),
-                       termion::clear::CurrentLine,
-                       name, value);
-                line += 1;
-            }
-        }
+		for ref pat in &ctmatch {
+		    if !pat.matches(&name) {
+			continue;
+		    }
+		    print!("{}{}|{}\t|{}\t|{}",
+			   termion::cursor::Goto(1, line as u16),
+			   termion::clear::CurrentLine,
+			   ifname, name, value);
+		    line += 1;
+		}
+	    }
+	}
 
-        stdout().flush().unwrap();
+	stdout().flush().unwrap();
         thread::sleep(time::Duration::from_millis(200));
     }
 }
