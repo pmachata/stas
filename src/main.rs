a3b416c200bda17f8a954dbeaefa34d8f3b87d6a
Petr Machata <pmachata@gmail.com>
2019-12-24 23:56:12 +0100

    Track counter history

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,19 @@ use std::time;
 mod ethtool_ss;
 mod netlink;
 
+#[derive(Eq, PartialEq)]
+struct CounterKey {
+    ifname: String,
+    ctns: String,
+    ctname: String,
+}
+
+struct CounterHistory {
+    key: CounterKey,
+    history: Vec<u64>,
+    age: u32,
+}
+
 fn main() {
     let ifmatch: glob::Pattern;
     let ctmatch: Vec::<glob::Pattern>;
@@ -39,29 +52,56 @@ fn main() {
 	ctmatch = _ctmatch;
     }
 
+    let mut state = Vec::<CounterHistory>::new();
     loop {
-	print!("{}", termion::clear::All);
-        let mut line = 1;
+
+	// Trim the history at 10 elements.
+	for entry in &mut state {
+	    entry.age += 1;
+	    if entry.age >= 10 {
+		entry.history.remove(0);
+	    }
+	}
+
+	// Counters that disappeared (e.g. due to their interface having disappeared) will
+	// eventually run out of history items. Drop them.
+	state = state.drain(..).filter(|entry| !entry.history.is_empty()).collect();
+
         for ifname in netlink::ifnames() {
             if !ifmatch.matches(&ifname) {
                 continue;
             }
 
-            for ethtool_ss::Stat{name, value} in ethtool_ss::stats_for(&ifname) {
-		for ref pat in &ctmatch {
-		    if !pat.matches(&name) {
-			continue;
-		    }
-		    print!("{}{}|{}\t|{}\t|{}",
-			   termion::cursor::Goto(1, line as u16),
-			   termion::clear::CurrentLine,
-			   ifname, name, value);
-		    line += 1;
+	    for stat in ethtool_ss::stats_for(&ifname).iter()
+		.filter(|ref stat|
+			ctmatch.iter().any(|ref pat|
+					   pat.matches(&stat.name)))
+	    {
+		let key = CounterKey { ifname: ifname.clone(),
+				       ctns: "ethtool".to_string(),
+				       ctname: stat.name.clone() };
+		if let Some(pos) = state.iter().position(|ref hist| hist.key == key) {
+		    state[pos].history.push(stat.value);
+		} else {
+		    state.push(CounterHistory{ age: 0, key: key,
+					       history: vec![stat.value] });
 		}
 	    }
 	}
 
+	print!("{}", termion::clear::All);
+        let mut line = 1;
+
+	for entry in &state {
+	    print!("{}{}|{}\t|{}\t|{}",
+		   termion::cursor::Goto(1, line as u16),
+		   termion::clear::CurrentLine,
+		   entry.key.ifname, entry.key.ctname,
+		   entry.history.last().unwrap());
+	    line += 1;
+	}
 	stdout().flush().unwrap();
-        thread::sleep(time::Duration::from_millis(200));
+
+        thread::sleep(time::Duration::from_millis(500));
     }
 }
