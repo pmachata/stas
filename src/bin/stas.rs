02db97eef38da3b028dd93e2cff7c2c3c0f108cc
Petr Machata <me@pmachata.org>
2020-02-23 00:00:13 +0100

    Introduce list filters
    List filters decide on ordering and inclusion of individual counter lines.
    This commit uses them to implement non-zero filter, but that is probably
    wrong, as the new syntax.txt note elaborates.
    
    Signed-off-by: Petr Machata <me@pmachata.org>

--- src/bin/stas.rs
+++ src/bin/stas.rs
@@ -18,21 +18,76 @@ struct CounterHistory {
     unit: stas::UnitChain,
 }
 
+fn show_help_exit(rc: i32) {
+    println!("Usage: stas [ethtool:] @eth* @ens* [...] tx_* rx_* /B/bps");
+    std::process::exit(rc);
+}
+
+struct CounterLine<'a> {
+    ifname: &'a str,
+    ctname: &'a str,
+    value: Option<stas::Value>,
+    avg: Option<stas::Value>,
+    freq: &'a stas::UFreq,
+    unit: stas::Unit,
+}
+
+trait CounterFilter {
+    fn filter<'a>(&self, counters: Vec<CounterLine<'a>>) -> Vec<CounterLine<'a>>;
+}
+
+struct NonZeroCounterFilter {}
+
+impl CounterFilter for NonZeroCounterFilter {
+    fn filter<'a>(&self, mut counters: Vec<CounterLine<'a>>) -> Vec<CounterLine<'a>> {
+        counters
+            .drain(..)
+            .filter(|cl| {
+                cl.value.unwrap_or(stas::Value::from_num(0)) != 0
+                    || cl.avg.unwrap_or(stas::Value::from_num(0)) != 0
+            })
+            .collect()
+    }
+}
+
 fn main() {
+    let mut counter_filters: Vec<Box<dyn CounterFilter>> = Vec::new();
     let rules;
 
     {
         let mut args: Vec<String> = env::args().collect();
         if args.len() <= 1 {
-            println!(
-                "Usage: {} [ethtool:] @eth* @ens* [...] tx_* rx_* /B/bps",
-                args[0]
-            );
-            std::process::exit(1);
+            show_help_exit(1);
         }
         args.remove(0);
 
-        match stas::parse_expr(&mut args.iter().peekable()) {
+        let mut it = args.iter().peekable();
+        while let Some(arg) = it.peek() {
+            match &arg[..] {
+                "--non0" => {
+                    counter_filters.push(Box::new(NonZeroCounterFilter {}));
+                    it.next();
+                }
+                "--once" => {
+                    println!("once");
+                    return;
+                    // xxx
+                    // it.next();
+                }
+                "--help" => {
+                    show_help_exit(0);
+                }
+                _ => {
+                    if arg.starts_with("-") {
+                        println!("Invalid argument {}", arg);
+                        show_help_exit(1);
+                    }
+                    break;
+                }
+            }
+        }
+
+        match stas::parse_expr(&mut it) {
             Ok(r) => rules = r,
             Err(e) => {
                 println!("Error: {}", e);
@@ -98,6 +153,48 @@ fn main() {
             }
         }
 
+        let mut counter_lines: Vec<CounterLine> = Vec::new();
+        for entry in &state {
+            // -1 for the first tick, which does not go into history.
+            let avg =
+                if (entry.history.len() >= (history_depth - 1) as usize) && entry.curr.is_some() {
+                    let mi = *entry.history.first().unwrap();
+                    let ma = entry.curr.unwrap();
+                    let d1 = stas::Value::from_num(ma) - stas::Value::from_num(mi);
+                    Some(d1 / stas::Value::from_num(avg_s))
+                } else {
+                    None
+                };
+
+            let value = match entry.unit.freq {
+                stas::UFreq::AsIs => entry.curr.map(|v| stas::Value::from_num(v)),
+                stas::UFreq::Delta => entry
+                    .curr
+                    .map(|v| stas::Value::from_num(v) - stas::Value::from_num(entry.base)),
+                stas::UFreq::PerSecond => match (entry.prev, entry.curr) {
+                    (Some(prev), Some(curr)) => Some(
+                        1000 * (stas::Value::from_num(curr) - stas::Value::from_num(prev))
+                            / stas::Value::from_num(cycle_ms),
+                    ),
+                    (_, _) => None,
+                },
+            };
+
+            let (value, avg, unit) = stas::convert(&entry.unit, value, avg);
+            counter_lines.push(CounterLine {
+                ifname: &entry.key.ifname,
+                ctname: &entry.key.ctname,
+                value: value,
+                avg: avg,
+                freq: &entry.unit.freq,
+                unit: unit,
+            });
+        }
+
+        for cf in &counter_filters {
+            counter_lines = cf.filter(counter_lines);
+        }
+
         print!("{}", termion::cursor::Goto(1, 1));
         let mut line = 1;
 
@@ -136,57 +233,30 @@ fn main() {
         print!("{}", termion::style::Reset);
 
         let mut last_ifname = "";
-        // -1 for the first tick, which does not go into history.
-        for entry in &state {
-            let avg =
-                if (entry.history.len() >= (history_depth - 1) as usize) && entry.curr.is_some() {
-                    let mi = *entry.history.first().unwrap();
-                    let ma = entry.curr.unwrap();
-                    let d1 = stas::Value::from_num(ma) - stas::Value::from_num(mi);
-                    Some(d1 / stas::Value::from_num(avg_s))
-                } else {
-                    None
-                };
-
-            let value = match entry.unit.freq {
-                stas::UFreq::AsIs => entry.curr.map(|v| stas::Value::from_num(v)),
-                stas::UFreq::Delta => entry
-                    .curr
-                    .map(|v| stas::Value::from_num(v) - stas::Value::from_num(entry.base)),
-                stas::UFreq::PerSecond => match (entry.prev, entry.curr) {
-                    (Some(prev), Some(curr)) => Some(
-                        1000 * (stas::Value::from_num(curr) - stas::Value::from_num(prev))
-                            / stas::Value::from_num(cycle_ms),
-                    ),
-                    (_, _) => None,
-                },
-            };
-
-            let (value, avg, unit) = stas::convert(&entry.unit, value, avg);
-
-            let unit_str = unit.base.to_string()
-                + match entry.unit.freq {
+        for counter_line in &counter_lines {
+            let unit_str = counter_line.unit.base.to_string()
+                + match counter_line.freq {
                     stas::UFreq::AsIs => "  ",
                     stas::UFreq::Delta => "  ",
                     stas::UFreq::PerSecond => "ps",
                 };
-            let unit_prefix_str = match entry.unit.freq {
+            let unit_prefix_str = match counter_line.freq {
                 stas::UFreq::AsIs => " ",
                 stas::UFreq::Delta => "\u{0394}",
                 stas::UFreq::PerSecond => " ",
             };
 
             line_out(
-                if last_ifname != entry.key.ifname {
-                    &entry.key.ifname
+                if last_ifname != counter_line.ifname {
+                    &counter_line.ifname
                 } else {
                     ""
                 },
-                &entry.key.ctname,
-                &if value.is_some() {
+                &counter_line.ctname,
+                &if counter_line.value.is_some() {
                     stas::humanize(
-                        value.unwrap(),
-                        unit.prefix,
+                        counter_line.value.unwrap(),
+                        counter_line.unit.prefix,
                         &unit_prefix_str,
                         &unit_str,
                         false,
@@ -194,14 +264,21 @@ fn main() {
                 } else {
                     "-     ".to_string()
                 },
-                &if avg.is_some() {
-                    stas::humanize(avg.unwrap(), unit.prefix, &unit_prefix_str, &unit_str, true)
+                &if counter_line.avg.is_some() {
+                    stas::humanize(
+                        counter_line.avg.unwrap(),
+                        counter_line.unit.prefix,
+                        &unit_prefix_str,
+                        &unit_str,
+                        true,
+                    )
                 } else {
                     "-     ".to_string()
                 },
             );
-            last_ifname = &entry.key.ifname;
+            last_ifname = &counter_line.ifname;
         }
+
         print!(
             "\n{}Overhead {:?}",
             termion::clear::CurrentLine,
